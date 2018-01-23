1f60bf75a62ca842bffb8528c3233a5ce39c78f2
mcjurij <sun.sucks@gmail.com>
2018-01-23 19:40:21 +0100

    try to find other omega sets when input length is bigger

--- modulo_mul.cpp
+++ modulo_mul.cpp
@@ -73,6 +73,29 @@ uint64_t pow_mod( uint64_t a, uint64_t b, uint64_t m)
 }
 
 
+static int find_inverse( int a, int m) // from Wikipedia
+{
+    int t= 0, newt = 1;
+    int r = m, newr = a;
+    
+    while( newr != 0 )
+    {
+        int quotient = r / newr;
+        int ht = t, hr = r;
+        t = newt;
+        newt = ht - quotient * newt;
+        r = newr;
+        newr = hr - quotient * newr;
+    }
+    
+    if( r > 1 )
+        return 0;  // :-/ :-( :-{
+    if( t < 0 )
+        t += m;
+    return t;
+}
+
+
 static unsigned bit_reverse( unsigned int in, unsigned int used_bits)
 {
     int r = 0;
@@ -103,56 +126,29 @@ static void unscramble( vector<mint> &v )
 }
 
 
-static const mint mod = 233;
+static mint mod = 0;
 
-static mint omega( int n )
+static mint omega_array[7];         // n = 2^7 max
+static mint inv_omega_array[7];
+
+static mint omega( int depth )
 {
-    int om = 0;
-    
-    switch( n )
-    {
-        case 2:
-            om = 232;
-            break;
-        case 4:
-            om = 144;
-            break;
-        case 8:
-            om = 12;
-            break;
-    }
-    
-    return om;
+    return omega_array[ depth ];
 }
 
 
-static mint inv_omega( int n )
+static mint inv_omega( int depth )
 {
-    int iom = 0;
-    
-    switch( n )
-    {
-        case 2:
-            iom = 232;
-            break;
-        case 4:
-            iom = 89;
-            break;
-        case 8:
-            iom = 136;
-            break;
-    }
-    
-    return iom;
+    return inv_omega_array[ depth ];
 }
 
 
-static void butterfly( vector<mint> &a, size_t s, size_t e, bool inverse)  // s and e are indexes
+static void butterfly( vector<mint> &a, size_t s, size_t e, int depth, bool inverse)  // s and e are indexes
 {
     int d = e-s;
     assert( d > 0 );
     int n = d+1;
-    mint om = inverse ? inv_omega( n ) : omega( n );
+    mint om = !inverse ? inv_omega( depth ) : omega( depth );
     int h = n/2;      // half
     int hs = s + h;   // half start
 
@@ -167,8 +163,8 @@ static void butterfly( vector<mint> &a, size_t s, size_t e, bool inverse)  // s 
 
     if( h > 1 )
     {
-        butterfly( a, s, hs - 1, inverse);
-        butterfly( a, hs, e, inverse);
+        butterfly( a, s, hs - 1, depth + 1, inverse);
+        butterfly( a, hs, e, depth + 1, inverse);
     }
 }
 
@@ -178,41 +174,19 @@ vector<mint> FFT( const vector<mint> &in, bool inverse=false)  // in's length mu
     vector<mint> Y = in;
     int n = Y.size();
 
-    butterfly( Y, 0, n-1, inverse);
+    butterfly( Y, 0, n-1, 0, inverse);
     unscramble( Y );
     
     return Y;
 }
 
 
-
-static int find_inverse( int a, int n) // from Wikipedia
-{
-    int t= 0, newt = 1;
-    int r = n, newr = a;
-    
-    while( newr != 0 )
-    {
-        int quotient = r / newr;
-        int ht = t, hr = r;
-        t = newt;
-        newt = ht - quotient * newt;
-        r = newr;
-        newr = hr - quotient * newr;
-    }
-    
-    if( r > 1 )
-        return 0;  // :-/ :-( :-{
-    if( t < 0 )
-        t += n;
-    return t;
-}
-
 vector<mint> IFFT( const vector<mint> &in )  // in's length must be 8
 {
     vector<mint> Y = FFT( in, true);
 
     mint d = find_inverse( Y.size(), mod);
+    assert( d != 0 );
     for( size_t i = 0; i < Y.size(); i++)
         Y[i] = mul_mod( Y[i], d, mod);
     
@@ -220,11 +194,70 @@ vector<mint> IFFT( const vector<mint> &in )  // in's length must be 8
 }
 
 
-vector<mint> mk_vector( const string &s, int n)
+bool pick_omegas( int n )
 {
-    int i, mlen = std::min( (int)s.length(), n);
+    int t;
+    int butterfly_depth;
+    bool found = false;
+    for( mod = 1000; mod < 100000 && !found; mod++)
+    {
+        for( t = 233; t < mod; t++)
+        {
+            int tn = n;
+            int om=t;
+            butterfly_depth = 0;
+            
+            while( pow_mod( om, tn, mod) == 1 && !found )
+            {
+                omega_array[ butterfly_depth++ ] = om;
+               
+                om = pow_mod( om, 2, mod);
+               
+                if( tn > 2 )
+                    tn >>= 1;
+                else
+                    found = true;
+            }
+            
+            if( found && omega_array[ butterfly_depth-1 ] == mod-1 )
+                break;
+            else
+                found = false;
+        }
+        
+        if( found )
+        {
+            int depth = 0;
+            for( depth = 0; depth < butterfly_depth; depth++)
+            {
+                int inv = find_inverse( omega_array[ depth ], mod);
+                if( inv != 0 && inv != 1 )
+                {
+                    inv_omega_array[ depth ] = inv;
+                }
+                else
+                {
+                    found = false;
+                    break;
+                }
+            }
+            
+            if( found && depth == butterfly_depth )
+                break;
+            else
+                found = false;
+        }
+    }
+    
+    return found;
+}
+
+
+vector<mint> mk_vector( const string &s )
+{
+    int i, mlen = (int)s.length();
     vector<mint> v;
-    v.resize( n );
+    v.resize( mlen );
     
     for( i = 0; i < mlen; i++)
     {
@@ -235,10 +268,20 @@ vector<mint> mk_vector( const string &s, int n)
             break;
     }
     
-    for( ; i < n; i++)
-        v[i] = 0;
-    
     return v;
+}
+
+
+vector<mint> fill_vector( const vector<mint> &v, int n)
+{
+    vector<mint> r = v;
+    size_t i = v.size();
+    r.resize( n );
+    
+    for( ; i < n; i++)
+        r[i] = 0;              // probably not needed?
+
+    return r;
 }
 
 
@@ -253,13 +296,41 @@ int main( int argc, char **argv)
     }
     
     string ps = argv[1];
-    string pq = argv[2];
+    string qs = argv[2];
+
+    vector<mint> pv = mk_vector( ps );
+    vector<mint> qv = mk_vector( qs );
+    int l = (int)std::max( pv.size(), qv.size());
     
-    P = FFT( mk_vector( ps, 8) );
-    Q = FFT( mk_vector( pq, 8) );
+    int bm = 1;
+    int bpos = 0;
+    while( l-bm > 0 )
+    {
+        bm <<= 1;
+        bpos++;
+    }
+
+    int n = 1<< (bpos + 1);
+    if( n > (1<<7) )
+    {
+        std::cerr << "input too large\n";
+        return 1;
+    }
+    
+    pv = fill_vector( pv, n);
+    qv = fill_vector( qv, n);
+    
+    if( !pick_omegas( n ) )
+    {
+        std::cerr << "no residual class found\n";
+        return 1;
+    }
+    
+    P = FFT( pv );
+    Q = FFT( qv );
     
     vector<mint> R;
-    R.resize( 8 );
+    R.resize( n );
     int i;
     for( i = 0; i < R.size(); i++)
         R[i] = mul_mod( P[i], Q[i], mod);
@@ -267,10 +338,10 @@ int main( int argc, char **argv)
     vector<mint> poly_coeff = IFFT( R );
     
     mint e = 1;
-    mint r = 0;
+    mint r = 0;                   // 64 bit is too small to hold sum
     for( i = 0; i < poly_coeff.size(); i++)
     {
-        r += poly_coeff[i] * e;
+        r += poly_coeff[i] * e;   // you need something bigger here ;-) 
         e *= 10;
     }
     cout << r << "\n";
